65fa79c68e6d9c268312085f4b0c999fd82fffbc
reynoalief <rardua@gmail.com>
2023-02-21 21:46:35 +0700

    feat: routing home and detail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminAdd from "./components/pages/admin-add";
+import AdminEdit from "./components/pages/admin-edit";
+import Home from "./components/pages/home";
+import MovieDetail from "./components/pages/movieDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
-        <Route path="/admin" />
+        <Route path="/" element={<Home />} />
+        <Route path="/admin-add" element={<AdminAdd />} />
+        <Route path="/admin-edit" element={<AdminEdit />} />
         <Route path="/sign-in" />
         <Route path="/sign-up" />
+        <Route path="/movie-detail" element={<MovieDetail />} />
       </Routes>
     </Router>
   );
